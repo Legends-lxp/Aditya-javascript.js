d971d03d2d99c4573f00abc906770466a684fa98
Legends-lxp <102133364+Legends-lxp@users.noreply.github.com>
2022-03-22 11:00:32 +0530

    Create Prawash.py

---
+++ Prawash.py
@@ -0,0 +1,17 @@
+java. 
+from await inpurt
+await run github.com/LEGEND-LX
+hack.google 
+outside:await str("hackx) -hacking 
+                  (inpurt)! 
+           aut 
+                  side 
+          hackin run await apaython
+                  java
+                  python (hackl
+  
+                        hacking java k. op
+                          
+                      ) 
+                        
+               
