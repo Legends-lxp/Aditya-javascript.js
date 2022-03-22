93cf45c5c97ac89dfe45a532c7c8da65ad47d82d
Legends-lxp <102133364+Legends-lxp@users.noreply.github.com>
2022-03-22 10:53:38 +0530

    Create Hacking tools java.js

---
+++ Hacking tools java.js
@@ -0,0 +1,24 @@
+inpurt python, java, php, linux, html, she'll, 
+from inpurt javascript
+run = hackscript.js
+
+}{\
+     
+  
+         inpurt ( java hacking tools;hack
+                    inpurt
+                 :await 
+             Await. edit. hacking)) 
+         (hacking.google:email.hack
+          
+          email.sending.) :await inpurt
+  setind_hack
+  seting. get. output=emails.google
+  awaits. get! hacking. not
+  sell
+  ) 
+  +inpurt running_output. 
+               javascript. sell. python. php
+  hacker= [Aditya pandit. ]{"hithikal hacking }
+                        ¶∆™
+                        
